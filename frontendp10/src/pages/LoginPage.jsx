2fe08bed727d710a756d9d9458286e64e5176c6c
7Flafleur <7nb@hotmail.fr>
2024-06-23 22:06:42 +0200

    router

--- frontendp10/src/pages/LoginPage.jsx
+++ frontendp10/src/pages/LoginPage.jsx
@@ -1,30 +1,41 @@
 import { useState } from "react";
 import { useHistory } from 'react-router-dom';
 
-
-
 export default function LogInPage() {
-
+  const [errorMessage, setErrorMessage] = useState('');
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
 
   const history = useHistory();
 
+  const onLogInClicked = async () => {
+    alert('Not implemented yet!')
+  }
 
   return (
     <div className="content-container">
       <h1>Log In</h1>
+      {errorMessage && <div className="fail">{errorMessage}</div>}
       <input 
-      value={emailValue}
-      onChange={e => setEmailValue(e.target.value)}
-      placeholder="someemail@someprovider.some" />
+        type="email"
+        value={emailValue}
+        onChange={e => setEmailValue(e.target.value)}
+        placeholder="someone@example.com" 
+      />
       <input
-      value={passwordValue}
-      onChange={e=> setPasswordValue(e.target.value)}
-       placeholder="password" />
-      <button>Log In</button>
-      <button>Frogot yopur password?</button>
-      <button>Sign Up</button>
+        type="password"
+        value={passwordValue}
+        onChange={e => setPasswordValue(e.target.value)}
+        placeholder="password" 
+      />
+      <button 
+        disabled={!emailValue || !passwordValue}
+        onClick={onLogInClicked}
+      >
+        Log In
+      </button>
+      <button onClick={() => history.push('/forgot-password')}>Forgot your password?</button>
+      <button onClick={() => history.push('/signup')}>Sign Up</button>
     </div>
   );
 }
